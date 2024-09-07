a82c1014b4851791cc5782354b88b1a06bb46d1f
kundup <kundup61@hotmail.com>
2024-09-06 21:25:05 -0700

    x direction platform collision added. restart button added and activated.

--- fraser.py
+++ fraser.py
@@ -12,7 +12,6 @@ sky = pygame.image.load("images/sky.png").convert_alpha()
 sky_image = pygame.transform.scale(sky, (screen_width, screen_height))
 sky_image_rect = sky_image.get_rect(topleft=(0, 0))
 sun_image = pygame.image.load("images/sun.png").convert_alpha()
-
 ghost_image = pygame.image.load("images/ghost.png")
 
 # font colors
@@ -40,6 +39,26 @@ def all_text(text, color, size, x, y):
     font_render = font_surf.render(text, False, color)
     font_rect = font_render.get_rect(midbottom=(x, y))
     screen.blit(font_render, font_rect)
+
+
+class Button:
+    def __init__(self):
+        self.restart = pygame.image.load("images/restart_btn.png")
+        self.rect = self.restart.get_rect(midbottom=(300, 350))
+        self.clicked = False
+
+    def draw(self):
+        pos = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0]:
+            if self.rect.collidepoint(pos):
+                player.reset()
+                self.clicked = True
+        else:
+            self.clicked = False
+
+        screen.blit(self.restart, self.rect)
+        return self.clicked
+
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -96,6 +115,7 @@ class Platform(pygame.sprite.Sprite):
 
         self.counter += 1
 
+
 class Lava(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
@@ -110,22 +130,7 @@ class Lava(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.index = 0
-        self.index_l = 0
-        self.direction = 0
-        self.vel_y = 0
-        self.vel_x = 2
-        self.jumped = False
-        img1 = pygame.image.load(f"images/guy1.png").convert_alpha()
-        img2 = pygame.image.load(f"images/guy2.png").convert_alpha()
-
-        self.img1 = [pygame.transform.scale(img1, (21, 47)), pygame.transform.scale(img2, (21, 47))]
-        self.img1_l = [pygame.transform.flip(num, True, False) for num in self.img1]
-
-        self.image = [self.img1, self.img1_l]
-        self.direction = 0
-        self.rect = self.image[self.direction][self.index].get_rect(
-            midbottom=(100, screen_height - tile_size * 2))
+        self.reset()
 
     def update_player(self, game_over):
 
@@ -174,6 +179,24 @@ class Player(pygame.sprite.Sprite):
             if pygame.sprite.spritecollide(self, enemy_group, False):
                 game_over = 1
 
+            for platforms in platform_group:
+                if platforms.rect.colliderect(self.rect.x + dx, self.rect.y, self.rect.width, self.rect.height) :
+                    dx = 0
+                # Yatay çarpışma
+                if platforms.rect.colliderect(self.rect.x, self.rect.y + dy, self.rect.width, self.rect.height):
+                    # oyuncu platformun altındaysa ve belirli bir mesafedeyse
+                    if self.vel_y >= 0:  # Oyuncu aşağı iniyorsa
+                        if abs(self.rect.bottom - platforms.rect.top) < self.threshold:
+                            self.rect.bottom = platforms.rect.top-1
+                            dy = 0
+                            self.vel_y = 0
+                            self.jumped = False
+                    elif self.vel_y < 0:  # Oyuncu yukarı çıkıyorsa
+                        if abs(self.rect.top - platforms.rect.bottom) < self.threshold:
+                            self.rect.top = platforms.rect.bottom-1
+                            dy = 0
+                            self.vel_y = 0
+
             self.rect.x += dx
             self.rect.y += dy
 
@@ -183,6 +206,7 @@ class Player(pygame.sprite.Sprite):
                 self.jumped = False
 
             screen.blit(self.image[self.direction][int(self.index)], self.rect)
+
         else:
             self.image = pygame.transform.smoothscale(ghost_image, (tile_size, tile_size))
             self.rect.y -= 2
@@ -190,6 +214,27 @@ class Player(pygame.sprite.Sprite):
             screen.blit(self.image, self.rect)
 
         return game_over
+
+    def reset(self):
+
+        self.index = 0
+        self.index_l = 0
+        self.direction = 0
+        self.vel_y = 0
+        self.vel_x = 2
+        self.jumped = False
+        self.threshold = 20
+
+        img1 = pygame.image.load(f"images/guy1.png").convert_alpha()
+        img2 = pygame.image.load(f"images/guy2.png").convert_alpha()
+
+        self.img1 = [pygame.transform.scale(img1, (21, 47)), pygame.transform.scale(img2, (21, 47))]
+        self.img1_l = [pygame.transform.flip(num, True, False) for num in self.img1]
+
+        self.image = [self.img1, self.img1_l]
+        self.direction = 0
+        self.rect = self.image[self.direction][self.index].get_rect(
+            midbottom=(100, screen_height - tile_size * 2))
 
 
 class WorldMap:
@@ -230,14 +275,12 @@ class WorldMap:
                     lava_group.add(lava)
 
                 if colomn == 6:
-                    platform = Platform(col_count * tile_size, row_count * tile_size, 1, 0 )
+                    platform = Platform(col_count * tile_size, row_count * tile_size, 1, 0)
                     platform_group.add(platform)
 
                 if colomn == 7:
                     platform = Platform(col_count * tile_size, row_count * tile_size, 0, 1)
                     platform_group.add(platform)
-
-
 
                 col_count += 1
             row_count += 1
@@ -267,7 +310,7 @@ world_map = [
     [1, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 2, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 5, 5, 2, 2, 0, 3, 0, 3, 0, 0, 0, 1],
+    [1, 7, 0, 0, 6, 0, 0, 0, 0, 2, 2, 2, 5, 5, 2, 2, 0, 3, 0, 3, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1],
     [1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -278,7 +321,7 @@ enemy_group = pygame.sprite.Group()
 coin_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
 platform_group = pygame.sprite.Group()
-
+button = Button()
 player = Player()
 worldmap = WorldMap(world_map)
 
@@ -299,6 +342,9 @@ while run:
         if player.rect.y <= 100:
             player.rect.y = 100
             all_text("Game Over", over_white, 80, 300, 300)
+
+            if button.draw():
+                game_over = 0
 
     else:
         enemy_group.update()
